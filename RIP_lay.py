97b532463354d7a2792c83b70af644e68a871fae
Eric Buonocore <45919390+ebuonocore@users.noreply.github.com>
2022-01-19 08:28:45 +0100

    Update RIP_lay.py

--- RIP_lay.py
+++ RIP_lay.py
@@ -1,31 +1,5 @@
 """ RIP_lay
     Simulation simplifiée du protocole RIP
-    Sur la base de l'exercice 5 du sujet zéro du baccalauréat
-    de la session 2021 de NSI
-    https://pixees.fr/informatiquelycee/term/suj_bac/2021/sujet_1.pdf
-    par Eric Buonocore le 18/01/2022
-
-    Objectifs:
-      + Visualiser l'ensemble des tables de routage d'un petit réseau
-      + Pas de notion d'adresses IP (ni de masques)
-      + Aborder les notions de passerelles, de route
-      + Faire le lien avec la notion de graphe
-      (noeuds/routeurs, arêtes/liaisons ou segmentde réseau)
-      + Visualiser la propagation de l'information dans les tables de routage
-      + Observer la stabilisation du système après des perturbations
-      + Visualiser la construction et la mise à jour des routes
-
-    Contraintes:
-      + Pour rester lisible: les noeuds doivent avoir des noms courts
-      (moins de 3 caractères)
-      + Le réseau ne doit pas compter un trop grand nombre de routeurs
-      (un dizaine au  maximum)
-
-    Sources images Freepix obtenues via flaticon.com pour les boutons:
-          + Annuler
-          + Configuration
-          + Fusée
-          + Paquet parachuté
 """
 
 import RIP_lay_classes as rc
